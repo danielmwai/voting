87a0040ba40229ca4582140912c1653f8d801592
danielmwai <daniel@nzaniela.com>
2018-09-13 18:50:03 +0300

    implementing the detail view ,  results view

--- ballots/views.py
+++ ballots/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http  import  HttpResponse, Http404
+from django.urls  import  reverse
+from django.http  import  HttpResponse, Http404, HttpResponseRedirect
 from django.template  import  loader
 from .models import Candidate, Party
 # Create your views here.
@@ -20,23 +21,28 @@ def  detail(request, party_id):
 
 
 def results(request, party_id):
-	response = 'party %s results' 
-	return HttpResponse( response % party_id)
+	# response = 'party %s results' 
+	# return HttpResponse( response % party_id)
+	party = get_object_or_404(Party, pk=party_id)
+	return  render(request, 'ballots/results.html',{
+		'party': party
+		})
 
 
 def vote_party(request, party_id):
 	party  = get_object_or_404(Party, pk=party_id)
-	print('********')
-	print(party)
+	print('Candidate  Post  Request', party.candidate_set.get(pk=request.POST['candidate']))
 	try:
-		selected_party = party.candidate_set.get(pk=request.POST['party'])
+		selected_candidate = party.candidate_set.get(pk=request.POST['candidate'])
+		print('**&%%^%$$%$')
+		print(selected_candidate)
 	except (KeyError, Candidate.DoesNotExist):
 		# Redisplay the canidate  ballot  form
 		return render(request, 'ballots/detail.html', {'party': party ,
 			  'error_message': 'You didnt select a  Candidate'})
 	else:
-		selected_party.votes +=1
-		selected_party.save()
+		selected_candidate.votes +=1
+		selected_candidate.save()
 
 		# Always  return  a  HttpResponseRedirect after  successful dealing with Post Data
 		# This prevents data  from being  posted twice  if a user hits the  Back Button

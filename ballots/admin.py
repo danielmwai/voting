b699aec71d7176ddef9b6fc9bf2ba5ce2611d2b3
danielmwai <daniel@nzaniela.com>
2018-09-13 19:31:08 +0300

    admin field and  fieldsets changes

--- ballots/admin.py
+++ ballots/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from.models import  Party, Candidate
 
-admin.site.register(Party)
-admin.site.register(Candidate)
+class  PartyAdmin(admin.ModelAdmin):
+	fields = ['party_name', 'party_registered_no', 'party_date_registered',]
+
+class CandidateAdmin(admin.ModelAdmin):
+	fieldsets = [
+	(None, {'fields': ['candidate', ]}),
+	('Candidate Info', {'fields': ['party','date_registered', 'votes']}),
+	]
+
+
+
+admin.site.register(Party, PartyAdmin)
+admin.site.register(Candidate, CandidateAdmin)
